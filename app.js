e14ddd9a11a68f25c8e7d3b598ed931092debe59
Richar Urbano <richar.urbano@bizagi.com>
2021-08-11 09:19:22 -0500

    Improvement to canvas

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 function drawGuideline(canvas) {
   if (!canvas || (canvas && !canvas.getContext)) return;
 
+  const width = canvas.width;
+  const height = canvas.height;
   const ctx = canvas.getContext("2d");
 
   ctx.lineWidth = 1;
 
-  drawLine(ctx, 0, 25, 650, "gray", 1, false);
-  drawLine(ctx, 0, 75, 650, "red", 1, true);
-  drawLine(ctx, 0, 125, 650, "blue", 1, false);
+  drawLine(ctx, 0, parseInt(height * 0.25), width, "gray", 1, false);
+  height * 0.25;
+  drawLine(ctx, 0, parseInt(height * 0.5), width, "red", 1, true);
+  drawLine(ctx, 0, parseInt(height * 0.75), width, "blue", 1, false);
 }
 
 function drawLine(
@@ -24,17 +27,14 @@ function drawLine(
 
   ctx.beginPath();
 
-  /*dashes are 5px and spaces are 3px*/
-  if (dashed) {
-    ctx.setLineDash([5, 3]);
-  } else {
-    ctx.setLineDash([]);
-  }
+  // Dashes are 5px and spaces are 3px
+  ctx.setLineDash(dashed ? [5, 3] : []);
+
   // For even stroke widths you can use integers for coordinates,
   // for odd stroke widths you want to use .5 to get crisp lines
   // that fill the pixels correctly.
   ctx.moveTo(x, y + 0.5);
   ctx.lineTo(x + length, y + 0.5);
-  ctx.closePath();
   ctx.stroke();
+  ctx.closePath();
 }
